aaf3145f7298108e08443b222a42f99a7ed3b838
crsfnts <crsfnts@gmail.com>
2025-10-18 22:10:29 -0700

    removal back to dash link
    removed  back to dashboard link

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -10,12 +10,12 @@ const BackButton: React.FC<BackButtonProps> = ({ setView }) => {
   return (
     <button
       onClick={() => setView(View.Dashboard)}
-      className="inline-flex items-center text-sm font-medium text-teal-600 hover:text-teal-800 mb-6 group"
+      className="text-teal-600 hover:text-teal-800 mb-4 transition-transform hover:-translate-x-1"
+      aria-label="Back to Dashboard"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 transition-transform group-hover:-translate-x-1" viewBox="0 0 20 20" fill="currentColor">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
         <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
       </svg>
-      Back to Dashboard
     </button>
   );
 };
